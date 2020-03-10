162f2c5867c6041feabc695a9cd3ddfe003ea133
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-03-10 08:57:27 -0400

    removed alert

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,3 @@
-// function activateGallery() {
-//   alert("Hello from the gallery file!");
-// }
-
 // Activates the image gallery.
 // The main task is to attach an event listener to each image in the gallery
 // and respond appropriately on click.
